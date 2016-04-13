7863daad4b245db7c882597d4de7685732d37686
gtgeorgiev <gtggeorgiev@abv.bg>
2016-04-13 14:01:51 +0300

    Change directory

---
+++ CSharp-part-1/4.Console Input And Output/02. Company Info/Program.cs
@@ -0,0 +1,40 @@
+﻿//A company has name, address, phone number, fax number, web site and manager. The manager has first name, last name, age and a phone number.
+//Write a program that reads the information about a company and its manager and prints it back on the console.using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace _02.Company_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string companyName = Console.ReadLine();
+            string companyAddress = Console.ReadLine();
+            string phoneNumber = Console.ReadLine();
+            string faxNumber = Console.ReadLine();
+            string webSite = Console.ReadLine();
+            string managerFirstName = Console.ReadLine();
+            string managerLastName = Console.ReadLine();
+            string managerAge = Console.ReadLine();
+            string managerPhone = Console.ReadLine();
+
+            if (faxNumber == "")
+            {
+                faxNumber = "(no fax)";
+            }
+
+            Console.WriteLine(companyName);
+            Console.WriteLine("Address: {0}", companyAddress);
+            Console.WriteLine("Tel. {0}", phoneNumber);
+            Console.WriteLine("Fax: {0}", faxNumber);
+            Console.WriteLine("Web site: {0}", webSite);
+            Console.WriteLine("Manager: {0} {1} (age: {2}, tel. {3})", managerFirstName, managerLastName, managerAge, managerPhone);
+
+        }
+    }
+}
