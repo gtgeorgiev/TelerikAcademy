fde6be2b5bd988670d03dc58f72d4c31a81f3669
gtggeorgiev <gtggeorgiev@abv.bg>
2016-04-21 21:48:16 +0300

    Solved first and second tasks from Loop's homework

---
+++ CSharp-part-1/6. Loops/01. Numbers from 1 to N/Program.cs
@@ -0,0 +1,20 @@
+﻿//Write a program that enters from the console a positive integer n and prints all the numbers from 1 to N inclusive, 
+//on a single line, separated by a whitespace.
+
+using System;
+
+namespace _01.Numbers_from_1_to_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= inputNumber; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+    }
+}
---
+++ CSharp-part-1/6. Loops/03. MMSA of N Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿//Write a program that reads from the console a sequence of N integer numbers and returns the minimal, the maximal number, 
+//the sum and the average of all numbers (displayed with 2 digits after the decimal point).
+
+//The input starts by the number N (alone in a line) followed by N lines, each holding an integer number.
+//The output is like in the examples below.
+
+using System;
+
+namespace _03.MMSA_of_N_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
