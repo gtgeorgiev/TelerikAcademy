c9886d3129e82fbf61578cafae8a055ff1443fe9
gtggeorgiev <gtggeorgiev@abv.bg>
2016-04-21 22:26:55 +0300

    Solved third task of Loop's homework

--- CSharp-part-1/6. Loops/03. MMSA of N Numbers/Program.cs
+++ CSharp-part-1/6. Loops/03. MMSA of N Numbers/Program.cs
@@ -12,6 +12,32 @@ namespace _03.MMSA_of_N_Numbers
     {
         static void Main(string[] args)
         {
+            int N = int.Parse(Console.ReadLine());
+
+            double max = Double.MinValue;
+            double min = Double.MaxValue;
+            double number = 0;
+            double sum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                number = double.Parse(Console.ReadLine());
+
+                sum += number; //storage of sum
+
+                if (number < min)
+                {
+                    min = number;
+                }
+
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+            Console.WriteLine(sum);
         }
     }
 }
