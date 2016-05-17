7b248eae35a6cb1e87731fb597ffa3441330fe93
gtggeorgiev <gtggeorgiev@abv.bg>
2016-05-17 21:42:40 +0300

    Solved fiveth and sixth tasks in Methods homework

---
+++ CSharp-part-2/03. Methods/05. Larger than neighbours/LargerThanNeighbours.cs
@@ -0,0 +1,42 @@
+﻿//Write a method that checks if the element at given position in given array of integers is larger than its two neighbours (when such exist). Write program that reads an array of numbers and prints how many of them are larger than their neighbours.
+
+using System;
+using System.Linq;
+
+
+class LargerThanNeighbours
+{
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        Console.WriteLine(FineLarger(numbers));
+    }
+
+    public static int FineLarger(int[] arr)
+    {
+        int number = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i != 0)
+            {
+                if (i == arr.Length - 2)
+                {
+                    if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+                    {
+                        number++;
+                    }
+                    break;
+                }
+
+                if (arr[i] > arr[i-1] && arr[i] > arr[i + 1])
+                {
+                    number++;
+                }
+                
+            }
+        }
+        return number;
+    }
+}
+
---
+++ CSharp-part-2/03. Methods/06. First larger than neighbours/FirstLargerThanNeighbours.cs
@@ -0,0 +1,47 @@
+﻿//Write a method that returns the index of the first element in array that is larger than its neighbours, or -1, if there is no such element.
+
+using System;
+using System.Linq;
+
+
+class FirstLargerThanNeighbours
+{
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        Console.WriteLine(FindIndexOfFirstLarger(numbers));
+    }
+
+    public static int FindIndexOfFirstLarger(int[] arr)
+    {
+        int index = -1;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i != 0)
+            {
+                if (i == arr.Length - 2)
+                {
+                    if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                if (arr[i] > arr[i-1] && arr[i] > arr[i + 1])
+                {
+                    index = i;
+                    break;
+                }
+            }
+        }
+
+        return index;
+    }
+
+
+    
+}
+
