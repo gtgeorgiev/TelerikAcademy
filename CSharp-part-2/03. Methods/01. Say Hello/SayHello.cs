250eff088ef8cec1f0675673a220171793df6e99
gtggeorgiev <gtggeorgiev@abv.bg>
2016-05-17 13:50:22 +0300

    Solved first and second tasks in Methods homework

---
+++ CSharp-part-2/03. Methods/01. Say Hello/SayHello.cs
@@ -0,0 +1,19 @@
+﻿//Write a method that asks the user for his name and prints Hello, <name>!. Write a program to test this method.
+
+using System;
+
+class SayHello
+{
+    static void Main(string[] args)
+    {
+        string inputName = Console.ReadLine();
+        PrintName(inputName);
+
+    }
+
+    public static void PrintName(string name)
+    {
+        Console.WriteLine("Hello, {0}!", name);
+    }
+}
+
---
+++ CSharp-part-2/03. Methods/02. Get largest number/GetTheLargestNumber.cs
@@ -0,0 +1,42 @@
+﻿//Write a method GetMax() with two parameters that returns the larger of two integers. Write a program that reads 3 integers from the console and prints the largest of them using the method GetMax().
+
+using System;
+using System.Linq;
+
+class GetTheLargestNumber
+{
+    static void Main(string[] args)
+    {
+        string inputLineOfIntigers = Console.ReadLine();
+        int[] stringToIntigers = inputLineOfIntigers.Split(' ').Select(int.Parse).ToArray();
+        int biggestNumber = int.MinValue;
+        int currentBigestNumber = int.MinValue;
+        for (int i = 0; i < stringToIntigers.Length; i++)
+        {
+            if (i != 0)
+            {
+                currentBigestNumber = GetMax(stringToIntigers[i], stringToIntigers[i - 1]);
+            }
+
+            if (currentBigestNumber > biggestNumber)
+            {
+                biggestNumber = currentBigestNumber;
+            }
+        }
+
+        Console.WriteLine(biggestNumber);
+    }
+
+    public static int GetMax(int number1, int number2)
+    {
+        int biggestNumber = number2;
+        if (number1 > number2)
+        {
+            biggestNumber = number1;
+        }
+
+        return biggestNumber;
+    }
+
+}
+
