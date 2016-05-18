13a174e065e14ebb51bf0933c27cc19458a12437
gtggeorgiev <gtggeorgiev@abv.bg>
2016-05-18 15:08:36 +0300

    Solved all tasks in Methods homework

---
+++ CSharp-part-2/03. Methods/07. Reverse number/ReverseNumbers.cs
@@ -0,0 +1,24 @@
+﻿//Write a method that reverses the digits of a given decimal number.
+
+using System;
+
+class ReverseNumber
+{
+    static string ReversedNumber(string number)
+    {
+        string reversed = string.Empty;
+        for (int i = number.Length - 1; i >= 0; i--)
+        {
+            reversed += number[i];
+        }
+
+        return reversed;
+    }
+
+    static void Main()
+    {
+        string numberToReverse = Console.ReadLine();
+
+        Console.WriteLine(ReversedNumber(numberToReverse));
+    }
+}
