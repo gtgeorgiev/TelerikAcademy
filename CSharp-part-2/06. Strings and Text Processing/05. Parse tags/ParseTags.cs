9b23f1aa3868ba8c563d66fd0d3bc82bcc287f54
gtggeorgiev <gtggeorgiev@abv.bg>
2016-06-03 13:36:25 +0300

    All tasks in String ana Text Processing homework

--- CSharp-part-2/06. Strings and Text Processing/05. Parse tags/ParseTags.cs
+++ CSharp-part-2/06. Strings and Text Processing/05. Parse tags/ParseTags.cs
@@ -1,37 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 class ParseTags
 {
     static void Main()
     {
-        var text = Console.ReadLine();
+        string text = Console.ReadLine();
 
-        var lengthOfFirstTag = "<upcase>".Length;
-
-        var indexX = text.IndexOf("<upcase>");
-        var indexY = text.IndexOf(@"</upcase>");
-
-        var indexXLength = text.IndexOf("<upcase>") + lengthOfFirstTag;
-
-        var lengthOfWord = indexY - indexXLength;
-        var textBetweenTheTags = text.Substring(indexXLength, lengthOfWord);
-        var upperText = textBetweenTheTags.ToUpper();
-        var fixedtext = text.Replace(textBetweenTheTags, upperText);
-        fixedtext = fixedtext.Replace("<upcase>", string.Empty);
-        fixedtext = fixedtext.Replace(@"</upcase>", string.Empty);
-
-        while (indexX != -1)
-        {
-            indexX = text.IndexOf("<upcase>", indexX + 1);
-            indexX + indexXLength
-        }
-
-        Console.WriteLine(fixedtext);
-        
-
+        Console.WriteLine(Regex.Replace(text, "<upcase>(.*?)</upcase>", word => word.Groups[1].Value.ToUpper()));
     }
 }
---
+++ CSharp-part-2/06. Strings and Text Processing/08. Extract Sentences/ExtractSentences.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+class ExtractSentences
+{
+    static void Main()
+    {
+        string pattern = Console.ReadLine();
+        string text = Console.ReadLine();
+
+        string[] splitted = text.Split('.');
+        char[] symbols = text.Where(x => !char.IsLetter(x)).Distinct().ToArray();
+
+        var result = new StringBuilder();
+        foreach (var sentence in splitted)
+        {
+            string[] words = sentence.Split(symbols);
+            if (words.Contains(pattern))
+            {
+                result.Append(sentence.Trim());
+                result.Append(". ");
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
